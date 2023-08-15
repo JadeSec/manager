3576ff7030e799c187145e9ebe22dddf3f5ca809
Renato Burton <renatoburton96@gmail.com>
2023-08-14 23:08:03 -0300

    Fix layout

---
+++ src/Application/App.Application.Project/Models/TeamPayload.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using App.Domain.Entities;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using App.Infra.CrossCutting.Helpers;
+using System.ComponentModel.DataAnnotations;
+using App.Infra.CrossCutting.Validators;
+
+namespace App.Application.Project.Models
+{
+    public class TeamPayload
+    {
+        [MinLength(1)]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        
+        [Image(width: 100, height: 100, new string[] { "image/jpeg" })]
+        public IFormFile Icon { get; set; }
+
+        [MinLength(20)]
+        [MaxLength(255)]
+        public string Description { get; set; }                     
+
+        public static async Task<ProjectTeamEntity> ToEntityAsync(TeamPayload payload)
+        {
+            return new ProjectTeamEntity()
+            {
+                Name = payload.Name,               
+                Description = payload.Description,
+                Icon = await FormFileHelper.ConvertToBase64Async(payload.Icon),
+                Created = DateTime.Now
+            };
+        }
+    }
+}
---
+++ src/Infra/CrossCutting/App.Infra.CrossCutting/Helpers/FormFileHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace App.Infra.CrossCutting.Helpers
+{
+    public static class FormFileHelper
+    {
+        public static async Task<string> ConvertToBase64Async(IFormFile formFile)
+        {
+            if (formFile == null || formFile.Length == 0)
+                return null;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await formFile.CopyToAsync(memoryStream);
+                var bytes = memoryStream.ToArray();
+                return Convert.ToBase64String(bytes);
+            }
+        }
+    }
+}
